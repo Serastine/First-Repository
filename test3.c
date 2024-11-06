53b9279d2c50ec746b7a728f6efa895ae6946389
Serastine <jyri.pakarinen@hotmail.com>
2024-11-06 21:56:06 +0200

    Create test3.c

---
+++ test3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  // Preprocessor directive to include standard input-output library
+
+int main() {  // Main function where the execution of the program starts
+    printf("Hello, World!\n");  // Print "Hello, World!" to the console
+    return 0;  // Return 0 to indicate that the program ended successfully
+}
